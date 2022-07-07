7f66553c130b26bc4607ca51240c7e2762fe9e39
IvanProkhorets <prohorets.ivan@gmail.com>
2022-07-07 16:09:35 +0300

    add simplelightbox and js code from hw-07

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,60 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const imagesMarkup = createImageGalleryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', imagesMarkup);
+gallery.addEventListener('click', onGalleryImageClick)
+
+function createImageGalleryMarkup (galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+        <a
+        class="gallery__link"
+        href="${original}"
+        onclick="event.preventDefault()">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+    </div>
+    `;
+    })
+    .join('');
+};
+
+function onGalleryImageClick(e) {
+    const openImage = e.target.classList.contains('gallery__image');
+
+    if (!openImage) {
+        return
+    } else {
+        
+        const instance = basicLightbox.create(`
+            <img src="${e.target.dataset.source}"  alt="${e.target.alt}">
+        `)
+        
+        instance.show()
+
+    function pressEsc(e) {
+
+        if (e.key === 'Escape') {
+            instance.close()
+        }
+    }
+        
+    document.addEventListener('keydown', pressEsc);
+    }
+}
