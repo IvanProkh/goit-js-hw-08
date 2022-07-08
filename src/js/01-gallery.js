0df61bb8a2244cc41fd2197184e8e7cec0a764a9
IvanProkhorets <prohorets.ivan@gmail.com>
2022-07-08 18:26:47 +0300

    add correct address

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,60 +1,39 @@
 // Add imports above this line
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
 
 const gallery = document.querySelector('.gallery');
-
 const imagesMarkup = createImageGalleryMarkup(galleryItems);
 
 gallery.insertAdjacentHTML('beforeend', imagesMarkup);
-gallery.addEventListener('click', onGalleryImageClick)
+gallery.addEventListener('click', onGalleryImageClick, { once: true });
+
+function onGalleryImageClick(e) {
+    console.log(e);
+    const lightbox = new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250, });
+}
 
 function createImageGalleryMarkup (galleryItems) {
     return galleryItems
         .map(({ preview, original, description }) => {
         return `
-        <div class="gallery__item">
-        <a
-        class="gallery__link"
+        <a 
+        class="gallery__item"
         href="${original}"
         onclick="event.preventDefault()">
-        <img
+            <img
             class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
+            src="${preview}" 
             alt="${description}"
-        />
+            />
         </a>
-    </div>
     `;
     })
     .join('');
 };
 
-function onGalleryImageClick(e) {
-    const openImage = e.target.classList.contains('gallery__image');
-
-    if (!openImage) {
-        return
-    } else {
-        
-        const instance = basicLightbox.create(`
-            <img src="${e.target.dataset.source}"  alt="${e.target.alt}">
-        `)
-        
-        instance.show()
-
-    function pressEsc(e) {
-
-        if (e.key === 'Escape') {
-            instance.close()
-        }
-    }
-        
-    document.addEventListener('keydown', pressEsc);
-    }
-}
